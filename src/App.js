40195bfb395b29fe45c15e72f6c0e708cac0c835
granly-del <ardiand400@gmail.com>
2023-03-09 20:57:55 +0800

    commit awal

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Grantly <code>src/App.js</code> Sorongan
         </p>
         <a
           className="App-link"
